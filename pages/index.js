a57c7afcf9dde0c9254a7aa35bdf5d2eff08ffea
mgracioli <michel-rg@hotmail.com>
2022-11-09 21:56:27 -0300

    Ateração dos arquivos de página inicial antes de adicionar rotas

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-import config from '../../config.json'
+import config from '../config.json'
 import StyledHome from './styles'
-import { Menu } from '../../src/components/Menu'
-import { Header } from '../../src/components/Header'
-import { Timeline } from '../../src/components/Timeline'
+import { Menu } from '../src/components/Menu'
+import { Header } from '../src/components/Header'
+import { Timeline } from '../src/components/Timeline'
 
 const Home = () => {
   const [valorDoFiltro, setValorDoFiltro] = useState('')
